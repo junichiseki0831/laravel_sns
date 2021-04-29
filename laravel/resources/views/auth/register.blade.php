cb7c3ec443bb7c195befef78141106c686425501
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-04-29 18:25:39 +0900

    googleアカウントで登録ボタン追加

--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -11,10 +11,13 @@
           <div class="card-body text-center">
             <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
 
+            <a href="{{ route('login.{provider}', ['provider' => 'google']) }}" class="btn btn-block btn-danger">
+              <i class="fab fa-google mr-1"></i>Googleで登録
+            </a>
+
             @include('error_card_list')
 
             <div class="card-text">
-              {{--ここから--}}
               <form method="POST" action="{{ route('register') }}">
                 @csrf
                 <div class="md-form">
@@ -36,7 +39,6 @@
                 </div>
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
               </form>
-              {{--ここまで--}}
 
               <div class="mt-0">
                 <a href="{{ route('login') }}" class="card-text">ログインはこちら</a>
